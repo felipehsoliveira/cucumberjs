50ae8998d4f2d3146391930d281c0f84fbdaa3a8
Marcus Ortense <marcus.ortense@outlook.com>
2017-02-01 15:59:48 -0200

    nightwatch configuration

--- config/nightwatch.chrome.conf.js
+++ config/nightwatch.chrome.conf.js
@@ -1,21 +1,20 @@
-require('babel-register')
-require('nightwatch-cucumber')
-// http://nightwatchjs.org/guide#settings-file
+require('nightwatch-cucumber')({
+  cucumberArgs: ['--require', 'helpers/timeout.js', '--require', 'features/step_definitions', '--format', 'pretty', '--format', 'json:reports/cucumber.json', 'features']
+})
+
 module.exports = {
-	src_folders: ['test/e2e/'],
-	output_folder: 'test/e2e/reports',
+	output_folder: '',
 	selenium: {
 		start_process: true,
 		server_path: 'node_modules/selenium-server/lib/runner/selenium-server-standalone-3.0.1.jar',
 		host: '127.0.0.1',
 		port: 4444,
-    cli_args: {
-		  'webdriver.gecko.driver': require('geckodriver').path,
-      'webdriver.chrome.driver': require('chromedriver').path
+		cli_args: {
+			'webdriver.chrome.driver': require('chromedriver').path
 		}
 	},
-
 	test_settings: {
+		default: {
 			launch_url: 'http://localhost',
 			selenium_port: 4444,
 			selenium_host: 'localhost',
--- config/nightwatch.firefox.conf.js
+++ config/nightwatch.firefox.conf.js
@@ -1,23 +1,18 @@
-require('babel-register')
 require('nightwatch-cucumber')({
-  cucumberArgs: ['--require', 'timeout.js', '--require', 'features/step_definitions', '--format', 'pretty', '--format', 'json:reports/cucumber.json', 'features']
+  cucumberArgs: ['--require', 'helpers/timeout.js', '--require', 'features/step_definitions', '--format', 'pretty', '--format', 'json:reports/cucumber.json', 'features']
 })
 
-// http://nightwatchjs.org/guide#settings-file
 module.exports = {
-	src_folders: [],
 	output_folder: '',
 	selenium: {
 		start_process: true,
 		server_path: 'node_modules/selenium-server/lib/runner/selenium-server-standalone-3.0.1.jar',
 		host: '127.0.0.1',
 		port: 4444,
-    cli_args: {
-		  'webdriver.gecko.driver': require('geckodriver').path,
-      'webdriver.chrome.driver': require('chromedriver').path
+		cli_args: {
+			'webdriver.gecko.driver': require('geckodriver').path
 		}
 	},
-
 	test_settings: {
 		default: {
 			launch_url: 'http://localhost',
@@ -29,7 +24,7 @@ module.exports = {
 				path: ''
 			},
 			desiredCapabilities: {
-				browserName: 'chrome',
+				browserName: 'firefox',
 				javascriptEnabled: true,
 				acceptSslCerts: true
 			}
